b485ef37ceb4eb45b7cee79b035faa1e9a660f5d
pawliux <pawliux@gmail.com>
2025-10-26 16:32:26 +0100

    flipping card using gsap.to

---
+++ pages/11b_challenge-from/script.js
@@ -0,0 +1,16 @@
+import gsap from 'gsap'
+
+const front = document.querySelector('.card-face.card-front')
+const back = document.querySelector('.card-face.card-back')
+const card =  document.querySelector('#card')
+let flipped = false
+
+card.addEventListener('click', (event)=>{
+    const flipped = gsap.getProperty(card, 'rotationY') === 180
+    gsap.to(card, {
+        rotationY: flipped ? 0 : 180,
+        duration: 0.5,
+        ease: 'power1.out'
+    })
+
+})
