1c0e29ce6f0337f9779825d3d69895cf52792eff
pawliux <pawliux@gmail.com>
2025-10-26 12:53:45 +0100

    toast challenge solved

--- pages/10b_challenge-to/script.js
+++ pages/10b_challenge-to/script.js
@@ -1,10 +1,37 @@
 import gsap from 'gsap'
 
 const toasts = document.querySelectorAll('.toast');
+let lorem;
+const toastTypes = ['one', 'two', 'three']
+const toastEmojis = ['🔔', '🎉', '🪓']
+
+const showToast = (text) => {
+    const toastContainer = document.querySelector('.demo section.flex-center');
+    const toast = document.createElement('div');
+    toast.classList.add('toast');
+    const toastTypeIndex = Math.floor(Math.random() * 3)
+    toast.classList.add(toastTypes[toastTypeIndex])
+    toast.innerText = `${toastEmojis[toastTypeIndex]} ${text}`;
+    toastContainer.appendChild(toast)
+    showToastLoop(toast)
+
+    // `<div class="toast one">🔔 You’ve got 99 problems</div>
+    //       <div class="toast two">🎉 And 101 merge conflicts</div>
+    //       <div class="toast three">🎉 And so on and on...</div>`
+}
+
+const getMessage = () => {
+    fetch('https://lorem-api.com/api/lorem')
+        .then(response => response.text())
+        .then((text) => {
+            let message = text.split(' ');
+            message = message.slice(0, 5)
+            showToast(message.join(' '));
+        })
+}
 
 
 const showToastLoop = (toast, i) => {
-    console.log(toast)
     gsap.to(toast, {
         opacity: 1,
         y: -120,
@@ -27,6 +54,10 @@ const showToastLoop = (toast, i) => {
 }
 
 
-toasts.forEach((toast, i) => {
-    showToastLoop(toast, i)
-})
+// toasts.forEach((toast, i) => {
+//     showToastLoop(toast, i)
+// })
+
+setInterval(() => {
+    getMessage()
+}, 5000)
